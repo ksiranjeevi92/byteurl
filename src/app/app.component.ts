2dfe981aa93b515e0db74fdcef46d45709cfa389
siranjeevi <ksiranjeevi92@gmail.com>
2026-01-02 14:28:34 +0530

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterModule],
 })
 export class AppComponent {
   title = 'byteurl';
